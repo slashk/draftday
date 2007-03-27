0e72f95d776544d316823119e5687db1a89b49a1
slashk <slashk@gmail.com>
2007-03-27 03:20:55 +0000

    rewrote the retainees (picks) file. added instructions to REAME file. added rank to picks.
    git-svn-id: svn+ssh://www.mtbcalendar.com/home/kpepple/svn/draftday/trunk@27 45e1ab1c-cbb3-db11-9a89-001422107726

--- db/create_picks.sql
+++ db/create_picks.sql
@@ -7,78 +7,23 @@
 # Generation Time: 2007-01-30 03:57:04 -0800
 # ************************************************************
 
-# Dump of table picks
-# ------------------------------------------------------------
 
-
+INSERT INTO `picks` VALUES 
 # Cows retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('8','1','1');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('11','1','2');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('19','1','3');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('288','1','4');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('273','1','5');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('265','1','6');
-
+(1,8,1,1),(2,11,1,2),(3,19,1,3),(4,288,1,4),(5,273,1,5),(6,265,1,6),
 # Coyotes retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('5','2','7');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('25','2','8');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('33','2','9');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('263','2','10');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('281','2','11');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('331','2','12');
-
+(7,5,2,7),(8,25,2,8),(9,33,2,9),(10,263,2,10),(11,281,2,11),(12,331,2,12),
 # Freds retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','3','13');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','3','14');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','3','15');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','3','16');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','3','17');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','3','18');
-
+(13,51,3,13),(14,50,3,14),(15,69,3,15),(16,285,3,16),(17,338,3,17),(18,275,3,18),
 # Pivos retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','4','19');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','4','20');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','4','21');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','4','22');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','4','23');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','4','24');
-
+(19,261,4,19),(20,268,4,20),(21,270,4,21),(22,4,4,22),(23,41,4,23),(24,47,4,24),
 # Rats retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','5','25');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','5','26');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','5','27');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','5','28');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','5','29');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','5','30');
-
+(25,3,5,25),(26,1,5,26),(27,22,5,27),(28,269,5,28),(29,279,5,29),(30,186,5,30),
 # Treehuggers retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','6','31');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','6','32');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','6','33');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','6','34');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','6','35');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','6','36');
-
+(31,2,6,31),(32,7,6,32),(33,13,6,33),(34,262,6,34),(35,272,6,35),(36,290,6,36),
 # Krunchers retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','7','37');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','7','38');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','7','39');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','7','40');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','7','41');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','7','42');
-
+(37,15,7,37),(38,30,7,38),(39,62,7,39),(40,336,7,40),(41,291,7,41),(42,314,7,42),
 # Possum retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','8','43');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','8','44');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','8','45');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','8','46');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','8','47');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','8','48');
-
+(43,10,8,43),(44,32,8,44),(45,23,8,45),(46,278,8,46),(47,284,8,47),(48,298,8,48),
 # GREMOBY retainees
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','9','49');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','9','50');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','9','51');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','9','52');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','9','53');
-INSERT INTO `picks` (`player_id`,`team_id`,`pick_number`) VALUES ('0','9','54');
+(49,12,9,49),(50,17,9,50),(51,46,9,51),(52,264,9,52),(53,304,9,53),(54,266,9,54);
