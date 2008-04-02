f5abf56414a0e96eece1cbb548bd89e4017f3bb0
slashk <slashk@gmail.com>
2008-04-02 04:09:09 +0000

    reimplemented the scaffold-ing that rails 2.0.2 cruelly ripped away
    git-svn-id: svn+ssh://www.mtbcalendar.com/home/kpepple/svn/draftday/trunk@56 45e1ab1c-cbb3-db11-9a89-001422107726

--- app/controllers/teamadmin_controller.rb
+++ app/controllers/teamadmin_controller.rb
@@ -1,3 +1,38 @@
 class TeamadminController < ApplicationController
-  #scaffold :team
+    def list
+    @teams = Team.find(:all)
+  end
+  
+  def edit
+    @team = Team.find(params[:id])
+  end
+  
+    def show
+    @team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(params[:team])
+    if @team.save
+      flash[:notice] = 'Team was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+  
+    def update
+    @team = Team.find(params[:id])
+    if @team.update_attributes(params[:team])
+      flash[:notice] = 'Team was successfully updated.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'edit'
+    end
+  end
+  
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -13,4 +13,40 @@ class UserController < ApplicationController
     redirect_to :controller => 'playeradmin', :action => 'live'
   end
   
+  def list
+    @users = User.find(:all)
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+    def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = 'User was successfully created.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'new'
+    end
+  end
+  
+     def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = 'Team was successfully updated.'
+      redirect_to :action => 'list'
+    else
+      render :action => 'edit'
+    end
+  end
+  
 end
